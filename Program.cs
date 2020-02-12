03a40ca68a03000296e2b3c0bda257d663932a06
Art <deckuser@hotmail.com>
2020-02-12 16:46:34 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace _Threading
+{
+    class SyncThread
+    {
+        object block = new object();
+
+        public void SyncThreadMethod()
+        {
+            lock (block)
+            {
+                for (int counter = 0; counter < 10; counter++)
+                {
+                    //lock (new object { })
+                    {
+                        Console.WriteLine("Thread # {0}, step {1}", Thread.CurrentThread.GetHashCode(), counter);
+                        Thread.Sleep(100);
+                    }
+                }
+                Console.WriteLine(new string('-', 20));
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyncThread syncThread = new SyncThread();
+
+            for (int i = 0; i < 3; i++)
+            {
+                new Thread(syncThread.SyncThreadMethod).Start();
+            }
+
+            Thread.Sleep(500);
+
+            Console.Out.WriteLine("kjsjhdvvlkzjdlvj");
+
+            Console.ReadKey();
+        }
+    }
+}
